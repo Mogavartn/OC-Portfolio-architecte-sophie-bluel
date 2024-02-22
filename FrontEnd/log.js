18b037c14182005ac1027fffd792660ec78e4665
Riiturii <riiturii@gmail.com>
2024-02-22 13:57:20 +0100

    Merge to Master

---
+++ FrontEnd/log.js
@@ -0,0 +1,65 @@
+// Sélection , liaison du FORM à une écoute d'événement
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+    // Bloque la soumission auto du formulaire
+    e.preventDefault();
+    // Récupération de l'email et mot de passe entré par l'utilisateur
+        const email= document.getElementById("email").value;
+        const password= document.getElementById("password").value;
+    // Envoi de la requête POST à l'API
+    const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",        
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+    // Ajout des infos d'identification dans le body de la requête
+                body: JSON.stringify({ email, password }),
+                });
+    // Si réponse ok
+    if (response.ok) {
+        // Récupération données de la réponse
+        const data = await response.json();
+        // Stockage Token dans la session utilisateur
+        sessionStorage.setItem("accessToken", data.token);
+        // Redirection utilisateur vers page d'accueil
+        window.location.replace("index.html");
+        // Si réponse échec
+    }   else if (response.status === 404) {
+            // Efface si erreur précédente présente
+            document.getElementById("error").innerHTML = "";
+            // Sélection élément HTML pour afficher message d'erreur
+            const connexion = document.getElementById("error");
+            // Création élément HTML p pour afficher message d'erreur
+            const error = document.createElement("p");
+            // texte du message d'erreur
+            error.innerText = `"Erreur dans l'email"`;
+            // Centrage texte du message d'erreur
+            error.style.textAlign = `center`;
+            // Définition couleur texte du message d'erreur
+            error.style.color = `red`;
+            // Ajout une marge en bas du message d'erreur
+            error.style.marginTop = `15px`;
+            // Insère le message d'erreur avant le dernier élément enfant de l'élément HTML sélectionné
+            connexion.insertBefore(error, connexion.lastElementChild);
+            }
+        else if (response.status === 401) {
+            // Efface si erreur précédente présente
+            document.getElementById("error").innerHTML = "";
+            // Sélection élément HTML pour afficher message d'erreur
+            const connexion = document.getElementById("error");
+            // Création élément HTML p pour afficher message d'erreur
+            const error = document.createElement("p");
+            // texte du message d'erreur
+            error.innerText = `"Erreur dans le mot de passe"`;
+            // Centrage texte du message d'erreur
+            error.style.textAlign = `center`;
+            // Définition couleur texte du message d'erreur
+            error.style.color = `red`;
+            // Ajout une marge en bas du message d'erreur
+            error.style.marginTop = `15px`;
+            // Insère le message d'erreur avant le dernier élément enfant de l'élément HTML sélectionné
+            connexion.insertBefore(error, connexion.lastElementChild);
+        }
+    })
---
+++ FrontEnd/works.js
@@ -0,0 +1,121 @@
+// Récupération des travaux eventuellement stockées dans le localStorage
+let works = window.localStorage.getItem('works');
+
+if (works === null){
+    // Récupération des travaux depuis l'API
+    const workReponse = await fetch('http://localhost:5678/api/works');
+    works = await workReponse.json();
+    // Transformation des travaux en JSON
+    const listWorks = JSON.stringify(works);
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("works", listWorks);
+}else{
+    works = JSON.parse(works);
+}
+
+// Récupération des catégories
+let categories = window.localStorage.getItem('categories');
+if (categories === null) {
+   const catReponse = await fetch('http://localhost:5678/api/categories');
+   categories = await catReponse.json();
+   const listCat = JSON.stringify(categories);
+   window.localStorage.setItem("categories", listCat);
+}else{
+    categories = JSON.parse(categories);
+}
+
+// Génération des travaux et intégration
+function genWorks(works){
+    for (let i = 0; i < works.length; i++) {
+
+        const figure = works[i];
+        // Récupération de l'élément du DOM qui accueillera les fiches figures
+        const sectionGallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un travail
+        const workElement = document.createElement("figure");
+        workElement.dataset.id = works[i].id
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        const titleElement = document.createElement("figcaption");
+        titleElement.innerText = figure.title;        
+        // Liaison de la balise figure a la section Gallery
+        sectionGallery.appendChild(workElement);
+        workElement.appendChild(imageElement);
+        workElement.appendChild(titleElement);    
+     }
+}
+
+genWorks(works);
+
+// Création et activation des boutons de filtres
+// Bouton affichage de tous projets
+const allButton = document.createElement("button");
+allButton.className = "filterButtons";
+filterBar.appendChild(allButton);
+allButton.textContent = "Tous";
+allButton.addEventListener("click", function () {
+    window.localStorage.removeItem("works");
+    document.querySelector(".gallery").innerHTML = "";
+    genWorks(works);
+});
+
+// Boutons par catégories
+for (let category of categories) {
+    const filterButtons = document.createElement("button");
+    filterButtons.className = "filterButtons";
+    filterButtons.dataset.id = category.id;
+    filterButtons.textContent = category.name;
+    filterBar.appendChild(filterButtons);
+
+    filterButtons.addEventListener("click", function () {
+    const worksObjects = works.filter(function (works) {
+        return works.category.id === category.id;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    genWorks(worksObjects);
+});
+
+}  
+
+
+/*filterButtons.addEventListener("click", function () {
+        const worksObjects = works.filter(function (works) {
+            return works.category.id === 1;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        genWorks(worksObjects);
+    });
+    }*/
+
+/*
+const objectsButton = document.querySelector(".btn-objects");
+
+objectsButton.addEventListener("click", function () {
+    const worksObjects = works.filter(function (works) {
+        return works.category.id === 1;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    genWorks(worksObjects);
+});
+
+const apartmentButton = document.querySelector(".btn-apartments");
+
+apartmentButton.addEventListener("click", function () {
+    const worksApartment = works.filter(function (works) {
+        return works.category.id === 2;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    genWorks(worksApartment);
+});
+
+const hotelsButton = document.querySelector(".btn-hotels");
+
+hotelsButton.addEventListener("click", function () {
+    const worksHotels = works.filter(function (works) {
+        return works.category.id === 3;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    genWorks(worksHotels);
+});
+*/
